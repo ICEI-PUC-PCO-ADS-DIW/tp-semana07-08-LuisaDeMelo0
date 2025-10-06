0fb570f611701ce6beba18830e99c7326d2d6ae3
Luísa de Melo <luisadmelo.fs@gmail.com>
2025-10-05 22:31:44 -0300

    criação da página de detalhes dinâmica

--- public/app.js
+++ public/app.js
@@ -2,10 +2,12 @@ const receitas = [
   { id: 1, titulo: "Receita 1", descricao: "Bolo de chocolate delicioso", imagem: "https://picsum.photos/id/101/600/400" },
   { id: 2, titulo: "Receita 2", descricao: "Salada refrescante", imagem: "https://picsum.photos/id/102/600/400" },
   { id: 3, titulo: "Receita 3", descricao: "Massa caseira", imagem: "https://picsum.photos/id/103/600/400" },
-  { id: 4, titulo: "Receita 4", descricao: "Sobremesa de frutas vermelhas", imagem: "https://picsum.photos/id/104/600/400" }
+  { id: 4, titulo: "Receita 4", descricao: "Sobremesa de frutas vermelhas", imagem: "https://picsum.photos/id/104/600/400" },
+  { id: 5, titulo: "Receita 5", descricao: "Frango grelhado com legumes", imagem: "https://picsum.photos/id/108/600/400" },
+  { id: 6, titulo: "Receita 6", descricao: "Panquecas fofinhas", imagem: "https://picsum.photos/id/106/600/400"},
 ];
 
-// Parte 1: cria cards dinâmicos na home
+
 if (window.location.pathname.includes('index.html')) {
   const container = document.getElementById('cards-container');
   receitas.forEach(r => {
@@ -23,4 +25,18 @@ if (window.location.pathname.includes('index.html')) {
     container.appendChild(card);
   });
 }
+
+if (window.location.pathname.includes('detalhes.html')) {
+  const params = new URLSearchParams(window.location.search);
+  const id = parseInt(params.get('id'));
+  const receita = receitas.find(r => r.id === id);
+
+
+  if (receita) {
+    document.getElementById('titulo').textContent = receita.titulo;
+    document.getElementById('descricao').textContent = receita.descricao;
+    document.getElementById('imagem').src = receita.imagem;
+  }
+}
+
 
